89c23a60b0466996d47fa55c0773ca914e600b2e
mikelabib <javaone123@yahoo.com>
2016-11-21 11:51:24 -0800

    login.php
    login screen - redirects to login.php with un/pw

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<title>Login Form in PHP with Session</title>
+<link href="style.css" rel="stylesheet" type="text/css">
+
+</head>
+
+<body>
+
+<div id="main">
+<h1>PHP Login Session Example</h1>
+<div id="login">
+<h2>Login Form</h2>
+
+<form action="login.php" method="post">
+
+  <label>UserName :</label>
+  <input id="name" name="username" placeholder="username" type="text">
+  <label>Password :</label>
+  <input id="password" name="password" placeholder="**********" type="password">
+  <input type ="submit">
+
+</form>
+
+</div>
+</div>
+</body>
+</html>
+
